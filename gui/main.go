24f680d36baa231be21c506245cc1cc29374108d
Gabor Szabo <gabor@szabgab.com>
2022-01-11 15:47:08 +0200

    without external function

--- gui/main.go
+++ gui/main.go
@@ -6,16 +6,13 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-func setText(obj widget.Label) {
-	obj.SetText("Welcome :)")
-}
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 
 	hello := widget.NewLabel("Hello Fyne!")
 	btn := widget.NewButton("Hi!", func() {
-		setText(*hello)
+		hello.SetText("Welcome :)")
 	})
 	w.SetContent(container.NewVBox(
 		hello,
