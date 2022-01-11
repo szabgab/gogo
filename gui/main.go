2ab988fb32ec5505fc0a1d4e93651323184c4ee2
Gabor Szabo <gabor@szabgab.com>
2022-01-11 16:38:52 +0200

    button to open another window

--- gui/main.go
+++ gui/main.go
@@ -1,30 +1,40 @@
 package main
 
 import (
-	"time"
+	"fmt"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Hello")
-	myWindow.SetContent(widget.NewLabel("Hello"))
 
-	go showAnother(myApp)
+	label := widget.NewLabel("Hello")
+	btn := widget.NewButton("Open", func() {
+		fmt.Println("open")
+		go showAnother(myApp)
+	})
+
+	myWindow.SetContent(container.NewVBox(
+		label,
+		btn))
+
 	myWindow.ShowAndRun()
 }
 
 func showAnother(a fyne.App) {
-	time.Sleep(time.Second * 5)
+	//time.Sleep(time.Second * 5)
 
 	win := a.NewWindow("Shown later")
-	win.SetContent(widget.NewLabel("5 seconds later"))
+	//win.SetContent(widget.NewLabel("5 seconds later"))
+	win.SetContent(widget.NewButton("Close", func() { win.Close() }))
 	win.Resize(fyne.NewSize(200, 200))
 	win.Show()
 
-	time.Sleep(time.Second * 2)
-	win.Close()
+	//time.Sleep(time.Second * 2)
+	//win.Close()
 }
