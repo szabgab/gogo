462874b1dcf8fd2da3e518afd7ee0acea0d0b27e
Gabor Szabo <gabor@szabgab.com>
2022-01-11 15:37:52 +0200

    button, second demo

--- gui/main.go
+++ gui/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
+	w := a.NewWindow("Hello")
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
 	w.ShowAndRun()
 }
