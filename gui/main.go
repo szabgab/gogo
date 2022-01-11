4840b7a2f4d1a8d6b0a8c326bbb62c6bfb023af6
Gabor Szabo <gabor@szabgab.com>
2022-01-11 18:27:53 +0200

    replace content of the window

--- gui/main.go
+++ gui/main.go
@@ -10,33 +10,52 @@ import (
 )
 
 var myWindow fyne.Window
+var cnt int
+var cases = [][]string{}
+
+func showMain() {
+	label := widget.NewLabel("Hello")
+	btn := widget.NewButton("Open", pressButton)
+	mainView := container.NewVBox(
+		label,
+		btn)
+
+	myWindow.SetContent(mainView)
+}
+
+func pressButton() {
+	fmt.Println("open\n----")
+	cnt += 1
+	cnt %= 2
+	//fmt.Println(cnt)
+	//fmt.Println(cases[cnt])
+	buttons := []*widget.Button{}
+	//buttons := []string{}
+	buttonsView := container.NewVBox()
+	for _, name := range cases[cnt] {
+		//fmt.Println(name)
+		copy := name[:]
+		btn := widget.NewButton(name, func() { fmt.Println(copy) })
+		buttons = append(buttons, btn)
+		buttonsView.Add(btn)
+		//buttons = append(buttons, name)
+	}
+	btn := widget.NewButton("Back", showMain)
+
+	buttonsView.Add(btn)
+	myWindow.SetContent(buttonsView)
+}
 
 func main() {
 	myApp := app.New()
 	myWindow = myApp.NewWindow("Hello")
 
-	label := widget.NewLabel("Hello")
-	btn := widget.NewButton("Open", func() {
-		fmt.Println("open")
-		go showAnother(myApp)
-	})
+	cases = [][]string{
+		[]string{"apple", "banana", "peach"},
+		[]string{"dolphin", "ant"},
+	}
+	cnt = 0
 
-	myWindow.SetContent(container.NewVBox(
-		label,
-		btn))
-
+	showMain()
 	myWindow.ShowAndRun()
-}
-
-func showAnother(a fyne.App) {
-	//time.Sleep(time.Second * 5)
-
-	win := a.NewWindow("Shown later")
-	//win.SetContent(widget.NewLabel("5 seconds later"))
-	win.SetContent(widget.NewButton("Close", func() { win.Close() }))
-	win.Resize(fyne.NewSize(200, 200))
-	win.Show()
-
-	//time.Sleep(time.Second * 2)
-	//win.Close()
 }
